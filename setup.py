5fcd03295089502b21724139ff07fa0ff4619d34
Christian <christian@openorion.org>
2024-07-17 19:36:09 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="orion-cli",
-    version="0.1.0",
+    version="0.1.1",
     description="Command-line tool for Open Orion PLM",
     author="Christian",
     author_email="christian@openorion.org",
